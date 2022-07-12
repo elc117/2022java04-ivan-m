e3083ee35f34eae603ed92d596957be529184013
Ivan Maidana <75757584+IvanMaidana@users.noreply.github.com>
2022-07-12 11:31:42 -0300

    Lista 4 java.

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+
+
+public class Pessoa {
+    int cpf;
+    String nome;
+    
+    Pessoa(int cpf, String nome){
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+//POSSIVEIS CLASS DERIVADAS
+ //CLIENTE PROPRIEDADES: ENDEREÇO
+ //EMPREGADO PROPRIEDADES: SALARIO,  PODE DERIVAR DISSO O VENDEDOR E O GERENTE 
